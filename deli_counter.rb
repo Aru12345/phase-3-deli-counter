d0d8716d31a1cc000de80f952e5f375756c1354b
Arundhati <arundhatiraicar12@gmail.com>
2022-02-01 23:31:14 +0530

    First test done

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+     if (katz_deli.empty?)
+       puts "The line is currently empty."
+     elsif (katz_deli.length>0)
+          puts "The line is currently "
+     end
+end
+
+def take_a_number(katz_deli,name)
+
+end
