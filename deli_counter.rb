b5ef10dc41fddec608c8dcc97efda26b2ecee583
Arundhati <arundhatiraicar12@gmail.com>
2022-02-02 09:25:58 +0530

    commit tree 12584170a5e05bdc5a99ff230c37d9b8a89d13be

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,8 +13,18 @@ def line(deli)
      end
 end
 
-def take_a_number(deli,name)
+def take_a_number(deli, name)
+     deli << name
+     puts "Welcome, #{name}. You are number #{deli.length} in line."
+   end
+   
+def now_serving(deli)
+     if  deli.empty?
+          puts "There is nobody waiting to be served!"
+     else
+       
+          puts "Currently serving #{deli.first}."
+          deli.shift
+     end
 
- deli << name
- puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
